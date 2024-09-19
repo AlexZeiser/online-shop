bf172caef8a7877c32234fa5e2b3a14a8938b241
Alex Zeiser <alex.zeiser.rest@gmail.com>
2024-09-19 09:02:13 +0200

    navbar update

---
+++ online-shop/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+
+
+
+ReactDom.render(<Navbar/>, document.getElementById('root'));
